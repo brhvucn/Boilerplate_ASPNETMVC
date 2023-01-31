9ffb9b76951e66d4f09db8d3268180162944d8ae
Brian Hvarregaard <brian@median.dk>
2023-01-31 12:49:46 +0100

    Creating a company

--- Boilerplate/CRM.Frontend/Controllers/CompanyController.cs
+++ Boilerplate/CRM.Frontend/Controllers/CompanyController.cs
@@ -26,10 +26,18 @@ namespace CRM.Frontend.Controllers
         public async Task<IActionResult> CreateCompany()
         {
             //get the input from the form elements in the view, denoted by the "name=" attribute
-            string name = Request.Form["companyName"];            
+            string name = Request.Form["companyName"];
+            string email = Request.Form["email"];
+            string street = Request.Form["street"];
+            string city = Request.Form["city"];
+            string zipcode = Request.Form["zipcode"];
             //create the create company request
             CreateCompanyRequest createCompanyRequst = new CreateCompanyRequest();
-            createCompanyRequst.Name = name;            
+            createCompanyRequst.Name = name;
+            createCompanyRequst.Street = street;
+            createCompanyRequst.Email = email;
+            createCompanyRequst.City = city;
+            createCompanyRequst.ZipCode = zipcode;
             //save
             await this.companyFacade.CreateCompany(createCompanyRequst);
             //redirect to the index page
